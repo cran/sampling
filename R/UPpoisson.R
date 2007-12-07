09aa689fa093d740f791d89ecd6b9495360b7128
Alina Matei <alina.matei@unine.ch>
2007-12-07 00:00:00 +0000

    version 0.9

--- R/UPpoisson.R
+++ R/UPpoisson.R
@@ -1,3 +1,10 @@
 "UPpoisson" <-
-function(pik) runif(length(pik))<pik
+function(pik,eps=1e-6) 
+{if(any(is.na(pik))) stop("there are missing values in the pik vector")
+list=pik>eps & pik < 1-eps
+pikb=pik[list]
+s=pik
+s[list]=runif(length(pikb))<pikb
+s
+}
 
--- R/UPsystematic.R
+++ R/UPsystematic.R
@@ -1,12 +1,13 @@
-"UPsystematic" <-
-function (pik) 
+"UPsystematic"<-function(pik,eps=1e-6) 
 {
-pik1=pik[pik>0 & pik< 1]
-N=length(pik1)
-a=(c(0,cumsum(pik1))-runif(1,0,1)) %% 1
-s1=as.integer(a[1:N]>a[2:(N+1)])
-s=pik
-s[pik>0 & pik< 1]=s1
+if(any(is.na(pik))) warning("there are missing values in the pik vector")
+list=pik > eps & pik < 1-eps
+pik1 = pik[list]
+N = length(pik1)
+a = (c(0, cumsum(pik1)) - runif(1, 0, 1))%%1
+s1 = as.integer(a[1:N] > a[2:(N + 1)])
+s = pik
+s[list] = s1
 s
 }
 
