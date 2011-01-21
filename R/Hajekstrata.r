2d2938664cb257ebf3f49870bb0022f0cca3ec88
Alina Matei <alina.matei@unine.ch>
2011-01-21 00:00:00 +0000

    version 2.4

--- R/Hajekstrata.r
+++ R/Hajekstrata.r
@@ -1,4 +1,4 @@
-HTstrata<-function (y, pik, strata, description=FALSE) 
+Hajekstrata<-function(y,pik,strata,N=NULL,type=c("total","mean"),description=FALSE) 
 {
     str <- function(st, h, n) .C("str", as.double(st), as.integer(h), 
         as.integer(n), s = double(n), PACKAGE = "sampling")$s
@@ -6,17 +6,23 @@ HTstrata<-function (y, pik, strata, description=FALSE)
     if(any(is.na(y))) stop("there are missing values in y")
     if(length(y)!=length(pik)) stop("y and pik have different sizes")
     if (is.matrix(y)) 
-        sample.size <- nrow(y) else sample.size <- length(y)
+        sample.size <- nrow(y)
+    else sample.size <- length(y)
+    if(!is.vector(N)) N=as.vector(N)
     h <- unique(strata)
+    if(length(N)!=length(h)) stop("N should be a vector with the length equal to the number of strata")
     s1 <- 0
     for (i in 1:length(h)) {
         s <- str(strata, h[i], sample.size)
-        est<-HTestimator(y[s == 1], pik[s == 1])
+        est<-Hajekestimator(y[s == 1], pik[s == 1],N=N[i],type=type)
         s1 <- s1 + est
         if(description)
-         cat("For stratum",i,",the Horvitz-Thompson estimator is:",est,"\n")
+         cat("For stratum ",i,", the Hajek estimator is:",est,"\n")
     }
 if(description)
-  cat("The Horvitz-Thompson estimator is:\n")
-as.vector(s1)
+  cat("The Hajek estimator is:\n")
+s1
 }
+
+
+
--- R/as_int.r
+++ R/as_int.r
@@ -1,12 +1,10 @@
 .as_int<-function(x)
 {if(!is.integer(x)) {
-            xo =x
-            x = round(x)
+            xo = round(x)
             if(any(x > .Machine$integer.max))
                 stop("the input has entries too large to be integer")
             if(!identical(TRUE, (ax <- all.equal(xo, x))))
-                warning("the sum of pik has been rounded to integer: ", ax)
-            storage.mode(x) <- "integer"
+                warning("the sum of pik is not integer")
         }
 x
 }
