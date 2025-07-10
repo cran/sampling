a73573ab537052751d475c16aa644d70363f9d1c
Alina Matei <alina.matei@unine.ch>
2025-07-10 17:20:02 +0000

    version 2.11

--- R/UPopips.r
+++ R/UPopips.r
@@ -1,13 +1,17 @@
-UPopips<-function(lambda, type=c("pareto","uniform","exponential"))
+UPopips<-function(lambda, type=c("pareto","uniform","exponential"),eps = 1e-6)
 {
-if(any(is.na(lambda))) stop("there are missing values in the lambda vector")
-n=sum(lambda)
-if(!(type %in% c("pareto","uniform","exponential"))) stop("the type argument is wrong")
-omega=runif(n)
-switch(type,pareto=order(omega*(1-lambda)/((1-omega)*lambda))[1:n],
-uniform=order(omega/lambda)[1:n], exponential=order(log(1-omega)/log(1-lambda)))[1:n]
+  if(anyNA(lambda)) stop("there are missing values in the lambda vector")
+  type <- match.arg(type)
+  list<- (lambda > eps & lambda < 1 - eps)
+  lambda1 <- lambda[list]
+  N <- length(lambda1)
+  n <-.as_int(sum(lambda1))
+  omega <- runif(N)
+  s<-lambda
+  s1<-switch(type,pareto=(omega*(1-lambda1)/((1-omega)*lambda1)),
+         uniform=omega/lambda1, exponential=log(1-omega)/log(1-lambda1))
+  s1<-order(s1)    
+  s1<-s1[seq_len(n)]
+  s[list][s1]<-1
+  s
 }
-
-
-
-
--- R/writesample.R
+++ R/writesample.R
@@ -1,38 +1,8 @@
-"writesample" <-
-function(n,N) 
+writesample<-function(n,N)
 {
-if(n==N) samples=rep(1,times=N) 
-else{ 
-x=numeric(N) 
-row=1 
-for(i in (n+1):N) row=row*i 
-k=1 
-for(i in 1:(N-n)) k=k*i 
-row=row/k 
-samples=matrix(0,row,N) 
-k=1 
-sol=0 
-x[1]=-1 
-while(k<=N && k>0) 
-{ 
-while(x[k]<1) 
-{ 
-x[k]=x[k]+1 
-s=0 
-for(i in 1:N) 
-s=s+x[i] 
-if(s==n) { 
-sol=sol+1 
-samples[sol,]=x 
-} 
-else 
-if(k<N){k=k+1 
-x[k]=-1 
-} 
-} 
-k=k-1 
-} 
-} 
-samples
+  cc<-choose(N,n)
+  P<-matrix(0,cc,N)
+  R<-combn(N,n)
+  for(i in seq_len(cc)) P[i,R[,i]]<-1
+  P
 }
-
